28c7be26ac7036ed228d8a4efe575cd0c89ffea1
Cooper Runstein <36021744+Cooper-Runstein@users.noreply.github.com>
2018-05-01 23:10:57 -0700

    Refactored, removed unnecessary logs
    Attempted another fix to increasing dice count array. Untested solution thus far.

--- liarsdice.js
+++ liarsdice.js
@@ -1,4 +1,5 @@
 const main = () => {
+
 //###########Document buttons and displays##############
 //displays
     let currentHandDisplay = document.querySelector("#currentHand");
@@ -29,197 +30,206 @@ const main = () => {
 
 //Button Listeners
     startButton.addEventListener('click', () => {
-        hideElements([startButton]);
-        displayElements([submit, playersInput, nameInput]);
+        displayAndHide([submit, playersInput, nameInput], [startButton]);
     });
+
     submit.addEventListener('click', ()=>{
-        let gameInitialValues = returnGameSettings();
+        let gameInitialValues = getGameSettings();
         if(gameInitialValues !== false){
             game(gameInitialValues);}else{
             console.log("false");
         }
     });
 
-    const returnGameSettings = ()=>{
-        let name = nameInput.value;
-        if (10 > playersInput.value > 0){
-            console.log(`playersInput.value = ${playersInput.value}`);
-            let numPlayers = playersInput.value;
-            hideElements([submit, playersInput, nameInput]);
-            return ([name, numPlayers]);
-        }return false;
-    };
-
-
     bluffButton.addEventListener('click', () => {
-        console.log(`Bluff initial currentPNum: ${PlayerNumber}`);
         challenger = table[0];
         displayChallengeStatus(true);
-        console.log(`Bluff called on `);
         determineChallengeResult(true);
         endRound();
     });
+
     nextPlayerButton.addEventListener('click', () => {
-        console.log('nextPlayerListener Initialized');
         hideElements([nextPlayerButton]);
         readyNextPlayer();
         returnNewPlayerNumber();
     });
+
     nextRoundButton.addEventListener('click', () => {
-        console.log('nextRoundListener Initialized');
         hideElements([nextRoundButton, test]);
         resetRoundVariables();
         displayRound();
         startNextRound();
         firstPlayer();
-        console.log(`nextRoundListener exited-> currentPnum: ${PlayerNumber}`);
-
     });
+
     declareButton.addEventListener('click', () => {
-        processBetValidity(faceInput.value, countInput.value);
-        hideElements([declareDisplay, declareButton, inputs]);
-        displayElements([result, faceDisplay]);
-        determineChallengeResult(displayChallengeStatus(returnTrueIfAIChallenges()));
+        if(processBetValidity(faceInput.value, countInput.value)){
+        determineChallengeResult(displayChallengeStatus(returnTrueIfAIChallenges()));}
     });
-    passButton.addEventListener('click', ()=>{
-        hideElements([passButton, bluffButton, spotOnButton]);
-        displayElements([nextPlayerButton]);
 
+    passButton.addEventListener('click', ()=>{
+        displayAndHide([nextPlayerButton], [passButton, bluffButton, spotOnButton]);
     });
+
 //TESTING AREA
     spotOnButton.addEventListener('click', () => {
         console.log('SpotOn called');
-        displayChallengeStatus(true);
+        console.log(checkSpotOn());
         displayElements([nextRoundButton]);
-        determineChallengeResult(true);
         endRound();
     });
+
     const checkSpotOn = () =>{
-        let checkArr = playerPlayAI();
-        console.log(`checking if spotOn -> occuranceArray: ${diceOnTableIndexedArray}`);
-        return !(diceOnTableIndexedArray[checkArr[0] -1] === checkArr[1])
+        return (diceOnTableIndexedArray[lastBet[0] -1] === lastBet[1])
     };
 
-    const createPlayers = (num)=>{
-        for (let i =0; i <num; i++){
-            let x = new Player();
-            x.addToTable();
-        }
-    };
-    let names = [
+
+   const names = [
         "Shirleen", "Kara", "Cleveland","Merri", "Conception", "Haley", "Florance", "Dorie", "Luella", "Vernia",
         "Freeman", "Katharina", "Charmain", "Graham", "Darnell", "Bernetta", "Inell", "Page", "Garnett", "Annalisa",
         "Brant", "Valda", "Viki", "Asuncion", "Moira", "Kaycee", "Richelle", "Elicia", "Eneida", "Evelynn"
     ];
 
 // ###############OBJECTS AT TABLE##############
-    const Player = function (name) {
-        this.player = false;
-        this.name = name || names[randomName()];
-        this.hand = [0, 0, 0, 0, 0];
-        this.roll = () => {
-            for (let i = 0; i < this.hand.length; i++) {
-                this.hand[i] = Math.floor(Math.random() * 6) + 1;
-            }
-            console.log(`${this.name} has rolled`);
-        };
-        this.addToTable = () => {
-            table.push(this);
-        };
-        this.addOccurrences = () =>{
-            for (let i = 0; i < this.hand.length; i++) {
-                diceOnTableIndexedArray[this.hand[i] - 1] += 1;
-            }
-            numberOfDie += this.hand.length;
-            console.log(`${this.name} has added die to occurences`);
-        };
-    };
+    class Player{
+        constructor(name)
+        {
+            this.player = false;
+            this.name = name || getRandomName();
+            this.hand = [0, 0, 0, 0, 0];
+            this.roll = () => {
+                for (let i = 0; i < this.hand.length; i++) {
+                    this.hand[i] = Math.floor(Math.random() * 6) + 1;
+                }
+                console.log(`${this.name} has rolled`);
+            };
+            this.addToTable = () => {
+                table.push(this);
+            };
+            this.addOccurrences = () => {
+                for (let i = 0; i < this.hand.length; i++) {
+                    diceOnTableIndexedArray[this.hand[i] - 1] += 1;
+                }
+                numberOfDie += this.hand.length;
+            };
+        }
+    }
+
     //Main Variables
     let table = [];
     let PlayerNumber;
     let currentHand;
     let currentPlayer;
     let lastBet = [0, 0];
-    let betMade = false;
     let betFaceOccurrence = 0;
     let diceOnTableIndexedArray = [0,0,0,0,0,0];
     let numberOfDie = 0;
-    let betIsTrue = false;
     let challenger;
     let challenged;
 
 
-    const randomName = ()=> {return Math.floor(Math.random() * Math.floor(30))};
-//#############Game Functions###################
-    const startGame = (initialValues) => {
-        console.log("startGame Function initialized");
-        let human = new Player(initialValues[0]);
-        human.addToTable(table);
-        createPlayers(initialValues[1]);
-        if (table[0] !== undefined) {
-            table[0].player = true;
-            console.log("Players have been sat");
-        } else {
-            console.log("There are no players at the table");
-        }
-        console.log('startGame success');
-        console.log(`#########Game has started###########`);
-    };
+    //generic functions
     const displayElements = (array) =>{
+
         for (let element = 0; element < array.length; element++){
-            array[element].style.display = 'block';
-        }
+        array[element].style.display = 'block';}
     };
+
     const hideElements = (array) =>{
         for (let element = 0; element < array.length; element++){
             array[element].style.display = 'none';
         }
     };
-//Player set up
-    const setUpNextPlayer = () => {
-        currentPlayer = table[PlayerNumber];
-        currentHand = currentPlayer.hand;
-        displayElements([currentPlayerDisplay, currentHandDisplay]);
-        if (currentPlayer.player === true) {
-            currentHandDisplay.innerHTML = `<h1 class="text-align">${currentHand}</h1>`;
-            currentPlayerDisplay.innerHTML = `<h1 class="text-align">${currentPlayer.name}</h1>`;
-            displayElements([declareDisplay, declareButton, inputs]);
-            hideElements([spotOnButton, bluffButton]);
 
-        } else {
-            displayElements([spotOnButton, bluffButton, passButton, result]);
-            result.innerHTML = "";
-            currentPlayerDisplay.innerHTML = `<h1 class="text-align">${currentPlayer.name} is playing</h1>`;
-            currentHandDisplay.innerHTML = `Your hand is: ${table[0].hand}`;
-            lastBet = playerPlayAI();
+    const displayAndHide = (arrayAdd, arrayDelete)=>{
+        displayElements(arrayAdd);
+        hideElements(arrayDelete);
+    };
+
+
+//#############Game Functions###################
+    const startGame = (initialValues) => {
+        createHumanPlayer(initialValues);
+        createAiPlayers(initialValues[1]);
+        if (table[0] !== undefined) {
+            table[0].player = true;}
+    };
+
+    const createHumanPlayer = (initialValues)=>{
+        let human = new Player(initialValues[0]);
+        human.addToTable(table);
+    };
+
+    const getGameSettings = ()=>{
+        let name = nameInput.value;
+        if (10 > playersInput.value > 0){
+            let numPlayers = playersInput.value;
+            hideElements([submit, playersInput, nameInput]);
+            return ([name, numPlayers]);
+        }return false;
+    };
+
+    const getRandomName = ()=> {
+        let index = Math.floor(Math.random() * Math.floor(30));
+        let name = names[index];
+        names.splice(index, 1);
+        return name;
+    };
+
+    const createAiPlayers = (num)=>{
+        for (let i =0; i <num; i++){
+            let x = new Player();
+            x.addToTable();
         }
     };
 
+//Player set up
+    const setUpNextPlayer = () => {
+       getNextPlayer();
+       displayElements([currentPlayerDisplay, currentHandDisplay]);
+       if (currentPlayer.player === true) {
+          setUpHumanTurn();
+        } else {
+           setUpAiTurn();
+        }
+    };
+
+
+    const setUpHumanTurn = ()=>{
+        currentHandDisplay.innerHTML = `<h1 class="text-align">${currentHand}</h1>`;
+        currentPlayerDisplay.innerHTML = `<h1 class="text-align">${currentPlayer.name}</h1>`;
+        displayAndHide([declareDisplay, declareButton, inputs], [spotOnButton, bluffButton]);
+    };
+
+    const setUpAiTurn = ()=>{
+        displayElements([spotOnButton, bluffButton, passButton, result]);
+        result.innerHTML = "";
+        currentPlayerDisplay.innerHTML = `<h1 class="text-align">${currentPlayer.name} is playing</h1>`;
+        currentHandDisplay.innerHTML = `Your hand is: ${table[0].hand}`;
+        lastBet = playerPlayAI();
+    };
+
+    const getNextPlayer = ()=>{
+        currentPlayer = table[PlayerNumber];
+        currentHand = currentPlayer.hand;
+    };
+
     const firstPlayer = () => {
-        console.log(`firstPlayer Function Initialized-> CurrentPNum: ${PlayerNumber}`);
         setUpNextPlayer();
         PlayerNumber += 1;
         returnNewPlayerNumber();
-        console.log(`firstPlayer function Exited-> CurrentPNum: ${PlayerNumber}`);
     };
 
     const readyNextPlayer = () => {
-        console.log(`nextPlayer function initial-> currentPNum: ${PlayerNumber}`);
         setUpNextPlayer();
         PlayerNumber += 1;
-        console.log(`NextPlayer Function Exited -> currentPNum: ${PlayerNumber}`);
     };
 
     const returnNewPlayerNumber = () => {
         if (PlayerNumber === table.length || PlayerNumber === undefined) {
-            console.log(`Zeroing function Initial-> currentPNum: ${PlayerNumber}`);
             PlayerNumber = 0;
-            console.log("Zerod Exit");
         } else if (PlayerNumber < 0) {
             PlayerNumber = table.length - 1;
-            console.log(`Zero Exit End Table -> currentPNum: ${PlayerNumber}`);
-        } else {
-            console.log(`Zero Exit -> currentPNum: ${PlayerNumber}`)
         }
     };
 
@@ -237,75 +247,70 @@ const main = () => {
     };
 
     const endRound = () => {
-        console.log("endRound Function initial");
         resetRoundVariables();
         test.innerHTML = "ROUND OVER";
         PlayerNumber -= 1;
-        console.log(`endRound function exited -> currentPNum: ${PlayerNumber}`);
     };
 
     const resetRoundVariables = () => {
         lastBet = [0,0];
-        betIsTrue = false;
-        betFaceOccurrence = [0,0,0,0,0,0];
+        betFaceOccurrence = 0;
+        numberOfDie = 0;
+        diceOnTableIndexedArray=[0,0,0,0,0,0];
         hideElements([passButton, bluffButton, spotOnButton, nextPlayerButton]);
-        console.log(`Round Cleared`);
     };
 
     const displayRound = () => {
         hideElements([result]);
-        console.log(`Round Displayed`);
     };
 
-//GAME PLAY FUNCTIONS
-    //Player Bets
-    const testFunction = () =>{
+//GAME PLAY FUNCTION
+    const testHandsFunction = () =>{
         console.log("##########TEST FUNCTION##########");
         for (let i =0; i < table.length; i++){
             console.log(table[i].hand );
         }
         console.log(diceOnTableIndexedArray);
     };
-    const checkIfBetValid = (face, count) => {
+
+    const getBetTruth = () => {
+        let face = lastBet[0];
+        let count = lastBet[1];
+        console.log(diceOnTableIndexedArray);
+        let betIsTrue = (diceOnTableIndexedArray[face - 1] >= count);
+        return (betIsTrue);
+    };
+
+    const processBetValidity = (face, count) => {
+        let bet = getBetIfValid(face, count);
+        if (bet !== false) {
+            lastBet = bet;
+            displayAndHide([result, faceDisplay], [declareDisplay, declareButton, inputs]);
+            test2.innerHTML = `${lastBet}`;
+            return true;
+        } else {
+            test2.innerHTML = "Not Valid Input";
+        }
+    };
+
+    const getBetIfValid = (face, count) => {
         let lastFace = lastBet[0];
         let lastCount = lastBet[1];
         if ((face > lastFace && count === lastCount) || count > lastCount) {
-            betMade = true;
             betFaceOccurrence = count;
             return [face, count];
         }
         return false;
     };
 
-    const getBetTruth = () => {
-        console.log(`checking bet -> lastBet: ${lastBet} `);
-        let face = lastBet[0];
-        let count = lastBet[1];
-        console.log(diceOnTableIndexedArray);
-        betIsTrue = (diceOnTableIndexedArray[face - 1] >= count);
-        return (betIsTrue);
-    };
-    const processBetValidity = (face, count) => {
-        if (checkIfBetValid(face, count) !== false) {
-            lastBet = checkIfBetValid(face, count);
-            test2.innerHTML = `${lastBet}`;
-        } else {
-            test2.innerHTML = "Not Valid Input";
-        }
-        console.log('Hand Declared');
-    };
     const reportBet = () =>{
-        if (getBetTruth() === true){
-            console.log("reportBet Exit-> checkBet is true");
-            return true;
-        }else{
-            console.log("reportBet Exit-> checkBet is false");
-            return false;
-        }
+        return getBetTruth();
     };
+
     const returnDieOnTableRatio = () => {
         return (numberOfDie - betFaceOccurrence)/numberOfDie;
     };
+
     const returnTrueIfAIChallenges = () => {
         let dif = returnDieOnTableRatio();
         if (dif < 0) {
@@ -320,9 +325,7 @@ const main = () => {
             return Math.random() > .9}
     };
 
-
     const displayChallengeStatus = (challenge) =>{
-        console.log("Reporting Challenge");
         if (challenge){
             challenged = currentPlayer;
             if (challenged.player === true){
@@ -339,15 +342,12 @@ const main = () => {
     };
 
     const determineChallengeResult = (challenge) =>{
-        console.log("Testing challenge");
         if (challenge){
-            displayElements([nextRoundButton, result]);
-            hideElements([nextPlayerButton]);
+            displayAndHide([nextRoundButton, result], [nextPlayerButton]);
             handleChallengeCheck(getBetTruth());
         }else {
             displayElements([nextPlayerButton]);
         }
-        console.log(`Challenge outcome reported`);
     };
 
     const handleChallengeCheck = (betBoolean)=>{
@@ -375,30 +375,23 @@ const main = () => {
 
     const checkIfEliminated = (betLoser)=>{
         if (returnIfLastDie(betLoser)){
-            handleLastDieLost(betLoser);
-        }
-
+            handleLastDieLost(betLoser);}
     };
 
     const removeDie = (player) =>{
         player.hand  = player.hand.splice(1);
     };
+
     //Computer bets
     const playerPlayAI = ()=> {
         console.log("Player is Playing");
         let pBT = playerBet();
         faceDisplay.innerHTML = `${currentPlayer.name} bets ${pBT}`;
-        console.log(pBT);
-        console.log(pBT);
-        console.log(pBT);
-        console.log(pBT);
-        console.log(pBT);
         test2.innerHTML=`Last Bet: ${lastBet}`;
         return pBT;
     };
 
     const countFaces = () =>{
-        console.log(`counting faces -> current Hand: ${currentHand}`);
         let currentHandInts = [0, 0, 0, 0, 0, 0];
         for (let i = 0; i < currentHand.length; i++) {
             currentHandInts[currentHand[i] - 1] += 1;
@@ -428,9 +421,7 @@ const main = () => {
         return bestHand
     };
 
-
     const aiCheckCurrentHandValidity = hand => {
-        console.log(`checking if hand is bigger -> bestHand: ${hand}`);
         return ((hand[0] > lastBet[0]  && hand[1] >= lastBet[1]) || hand[1] > lastBet[1])
     };
 
